bc4adb76f11760cdcee366d0d0603e2cd2d37aa8
Sean Casey <sean@caseylabs.com>
2023-01-21 17:07:32 -0800

    Updates

--- kc-bash.sh
+++ kc-bash.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-HELP_TEXT=""
+# Usage:
+# . <(curl -s https://raw.githubusercontent.com/CaseyLabs/kc-bash/main/kc-bash.sh)
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+msg() {
+  echo >&2 -e "${1-}"
+}
 
 # Functions
 # ---------
@@ -8,10 +15,10 @@ HELP_TEXT=""
 dirExists() {
   if [[ -d "$1" ]]
   then
-    echo "[Info] Directory ($1) exists."
+    msg "[Info] Directory ($1) exists."
     return 0
   else
-    echo "[Error] Directory ($1) does NOT exist."
+    msg "[Error] Directory ($1) does NOT exist."
     return 1
   fi
 }
@@ -19,10 +26,10 @@ dirExists() {
 fileExists() {
   if [[ -f "$1" ]]
   then
-    echo "[Info] File ($1) exists."
+    msg "[Info] File ($1) exists."
     return 0
   else
-    echo "[Error] File ($1) does NOT exist."
+    msg "[Error] File ($1) does NOT exist."
     return 1
   fi
 }
